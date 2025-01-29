1c93acf90a69a0ba376bd498af9df83620c38e90
Moses Ugwu <ugwumoses1992@gmail.com>
2025-01-29 11:21:51 +0100

    installing ghrunner

--- Docker/infrastructure/locals.tf
+++ Docker/infrastructure/locals.tf
@@ -7,13 +7,13 @@ locals {
       username       = "mosesugwu"
       size           = "Standard_F2"
     },
-    # vm-2 = {
-    #   name           = data.azurecaf_name.vm_2.result
-    #   size           = "Standard_F2"
-    #   public_key     = file("moseskey.pub")
-    #   admin_username = "mosesugwu"
-    #   username       = "mosesugwu"
-    # },
+    vm-2 = {
+      name           = ghrunner1
+      size           = "Standard_F2"
+      public_key     = file("moseskey.pub")
+      admin_username = "mosesugwu"
+      username       = "mosesugwu"
+    },
     # vm-3 = {
     #   name           = data.azurecaf_name.vm_3.result
     #   size           = "Standard_F2"
@@ -29,11 +29,11 @@ locals {
       public_ip_address_id = module.virtual_network.pip_id 
 
     },
-    # vm-2 = {
-    #   name                 = data.azurecaf_name.nic_2.result
-    #   public_ip_address_id = azurerm_public_ip.vm_2.id
+    vm-2 = {
+      name                 = data.azurecaf_name.nic_2.result
+      public_ip_address_id = azurerm_public_ip.vm_2.id
 
-    # },
+    },
     # vm-3 = {
     #   name                 = data.azurecaf_name.nic_3.result
     #   public_ip_address_id = azurerm_public_ip.vm_3.id
--- Docker/infrastructure/main.tf
+++ Docker/infrastructure/main.tf
@@ -73,14 +73,14 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# resource "azurerm_public_ip" "vm_2" {
-#   name                = "vm2PublicIP"
-#   resource_group_name = module.resource_group.rg_name
-#   location            = module.resource_group.rg_location
-#   allocation_method   = "Static"
+resource "azurerm_public_ip" "vm_2" {
+  name                = "ghrunnerIP"
+  resource_group_name = module.resource_group.rg_name
+  location            = module.resource_group.rg_location
+  allocation_method   = "Static"
 
 
-# }
+}
 
 # resource "azurerm_public_ip" "vm_3" {
 #   name                = "vm3PublicIP"
