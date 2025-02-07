a3d94b88cb3381101520b26bc383c7686b4a9158
Moses Ugwu <ugwumoses1992@gmail.com>
2025-02-07 11:31:02 +0100

    github actions

---
+++ Docker/infrastructure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-moses-state"
+    storage_account_name = "mosesstore"
+    container_name = "terraformstate"
+    key = "dev.terraform.tfstate"
+  }
+}
