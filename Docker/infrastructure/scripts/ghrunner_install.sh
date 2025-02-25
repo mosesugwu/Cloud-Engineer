2f567141657f571217986757abcce5ebd84d7267
Moses Ugwu <ugwumoses1992@gmail.com>
2025-02-25 12:00:11 +0100

    testing out ghrunner install

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -14,7 +14,7 @@ TOKEN="$(TOKEN)"
 echo "RUNNER_URL: ${RUNNER_URL}"
 echo "RUNNER_SHA: ${RUNNER_SHA}"
 echo "RUNNER_TAR: ${RUNNER_TAR}"
-echo "TOKEN:       ${TOKEN}"
+echo "TOKEN: ${TOKEN}"
 
 #Debug: Print current directory
 echo "Current directory: $(pwd)"
@@ -31,7 +31,7 @@ curl -L -X POST -H "Accept: application/vnd.github+json" \
     https://api.github.com/repos/OWNER/REPO/actions/runners/registration-token > response.json 
 
 RUNNER_TOKEN=$(jq -r '.token' response.json)
-echo "RUNNER_TOKEN: ${RUNNER_TOKEN}"
+echo "RUNNER_TOKEN: $RUNNER_TOKEN"
 
 
 ./config.sh --url https://github.com/mosesugwu/Cloud-Engineer --token $RUNNER_TOKEN
