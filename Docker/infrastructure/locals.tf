8581680bcf3d7ed459b1a4be471e336b1b634bd0
Moses Ugwu <ugwumoses1992@gmail.com>
2025-02-27 12:36:48 +0100

    automate runner install

--- Docker/infrastructure/locals.tf
+++ Docker/infrastructure/locals.tf
@@ -1,14 +1,14 @@
 locals {
   virtual_machines = {
-    vm-1 = {
-      name           = "resumeapp-vm01"
-      public_key     = file("moseskey.pub")
-      admin_username = "mosesugwu"
-      username       = "mosesugwu"
-      size           = "Standard_F2"
-      custom_data    = local.custom_data_vm_1
-      vars           = {}
-    },
+    # vm-1 = {
+    #   name           = "resumeapp-vm01"
+    #   public_key     = file("moseskey.pub")
+    #   admin_username = "mosesugwu"
+    #   username       = "mosesugwu"
+    #   size           = "Standard_F2"
+    #   custom_data    = local.custom_data_vm_1
+    #   vars           = {}
+    # },
     vm-2 = {
       name           = "ghrunner-vm02"
       size           = "Standard_F2"
@@ -23,45 +23,45 @@ locals {
         TOKEN      = var.TOKEN
       }
     },
-    vm-3 = {
-      name           = "hashicorpvault-vm03"
-      size           = "Standard_F2"
-      public_key     = file("moseskey.pub")
-      admin_username = "mosesugwu"
-      username       = "mosesugwu"
-      custom_data    = local.custom_data_vm_1
-      vars           = {}
+    # vm-3 = {
+    #   name           = "hashicorpvault-vm03"
+    #   size           = "Standard_F2"
+    #   public_key     = file("moseskey.pub")
+    #   admin_username = "mosesugwu"
+    #   username       = "mosesugwu"
+    #   custom_data    = local.custom_data_vm_1
+    #   vars           = {}
 
 
-    }
+    # }
   }
   network_interface_ids = {
-    vm-1 = {
-      name                 = data.azurecaf_name.nic_1.result
-      public_ip_address_id = azurerm_public_ip.vm_1.id
-      subnet_id            = module.subnet.snet_id
+    # vm-1 = {
+    #   name                 = data.azurecaf_name.nic_1.result
+    #   public_ip_address_id = azurerm_public_ip.vm_1.id
+    #   subnet_id            = module.subnet.snet_id
 
-    },
+    # },
     vm-2 = {
       name                 = data.azurecaf_name.nic_2.result
       public_ip_address_id = azurerm_public_ip.vm_2.id
       subnet_id            = module.subnet.snet_id
 
     },
-    vm-3 = {
-      name                 = data.azurecaf_name.nic_3.result
-      public_ip_address_id = azurerm_public_ip.vm_3.id
-      subnet_id            = module.subnet.snet_id
+    # vm-3 = {
+    #   name                 = data.azurecaf_name.nic_3.result
+    #   public_ip_address_id = azurerm_public_ip.vm_3.id
+    #   subnet_id            = module.subnet.snet_id
 
-    }
+    # }
 
   }
 
-  db_name          = "mosesdb"
-  custom_data_vm_1 = var.custom_data_vm_1
+  # db_name          = "mosesdb"
+  # custom_data_vm_1 = var.custom_data_vm_1
   custom_data_vm_2 = var.custom_data_vm_2
-  custom_data_vm_3 = var.custom_data_vm_3
-  disk_name        = "hashidisk"
+  # custom_data_vm_3 = var.custom_data_vm_3
+  # disk_name        = "hashidisk"
 }
 
 
--- Docker/infrastructure/output.tf
+++ Docker/infrastructure/output.tf
@@ -1,7 +1,7 @@
-output "vm_1" {
-  value = module.virtual_network.ip_address
+# output "vm_1" {
+#   value = module.virtual_network.ip_address
 
-}
+# }
 
 output "vm_2" {
   value = azurerm_public_ip.vm_2.ip_address
@@ -9,7 +9,7 @@ output "vm_2" {
 }
 
 
-output "vm_3" {
-  value = azurerm_public_ip.vm_3.ip_address
+# output "vm_3" {
+#   value = azurerm_public_ip.vm_3.ip_address
 
-}
+# }
