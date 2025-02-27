fd65433777d1f6d47e1eec2707227e3da1039d00
Moses Ugwu <ugwumoses1992@gmail.com>
2025-02-27 13:32:44 +0100

    automate runner install

--- Docker/infrastructure/data.tf
+++ Docker/infrastructure/data.tf
@@ -51,6 +51,13 @@ data "template_file" "custom_data" {
   template = file(each.value.custom_data)
   vars     = each.value.vars
 }
+data "template_file" "custom_data" {
+  template = file("${path.module}/scripts/ghrunner_install.sh")
+  vars = {
+    OWNER = var.OWNER
+  }
+}
+
 
 # data "template_file" "custom_data" {
 #   for_each = local.virtual_machines
