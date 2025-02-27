079ad96404617c316f3f8aaeaa7f2b3281da0252
Moses Ugwu <ugwumoses1992@gmail.com>
2025-02-27 13:46:27 +0100

    automate runner install

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -39,7 +39,7 @@ tar xzf "${RUNNER_TAR}"
 
 curl -L -X POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $(TOKEN)" -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/mosesugwu/${REPO}/actions/runners/registration-token > response.json 
+    https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token > response.json 
 
 # Ensure response.json is owned by the current user
 sudo chown $USER:$USER response.json
