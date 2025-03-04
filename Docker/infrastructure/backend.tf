0719a894e6ef93d724710f376b5aa6f5c17dabcf
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-04 14:16:52 +0100

    automate runner install

--- Docker/infrastructure/backend.tf
+++ Docker/infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-moses-state"
-    storage_account_name = "mosesstore"
+    storage_account_name = "mosesstor"
     container_name       = "terraformstate"
     key                  = "dev.terraform.tfstate"
   }
