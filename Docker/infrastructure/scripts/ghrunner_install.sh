a60a48cda04a451b2512aeea631057e9066d978f
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-05 13:16:20 +0100

    test and automate ghrunner script

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -34,7 +34,7 @@ echo $PWD
 
 
 curl -o actions-runner-linux-x64-2.322.0.tar.gz -L "${RUNNER_URL}"
-echo "${RUNNER_SHA}  actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
+echo "${RUNNER_SHA} actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
 tar xzf "${RUNNER_TAR}"
 
 curl -L -X POST -H "Accept: application/vnd.github+json" \
