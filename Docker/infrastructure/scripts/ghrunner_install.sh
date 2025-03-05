8e469513197120f51bc37985ba94d14a3b4b118d
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-05 10:11:57 +0100

    test and automate runner install

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -37,15 +37,15 @@ curl -o actions-runner-linux-x64-2.322.0.tar.gz -L "${RUNNER_URL}"
 echo "${RUNNER_SHA}  actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
 tar xzf "${RUNNER_TAR}"
 
-# curl -L -X POST -H "Accept: application/vnd.github+json" \
-#     -H "Authorization: Bearer $(TOKEN)" -H "X-GitHub-Api-Version: 2022-11-28" \
-#     https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token > response.json 
+curl -L -X POST -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $(TOKEN)" -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token > response.json 
 
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $(TOKEN)" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token > response.json
+# curl -L \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer $(TOKEN)" \
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token > response.json
 
 # Ensure response.json is owned by the current user
 sudo chown $USER:$USER response.json
