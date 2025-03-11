104aa05e90ac4078409dc7ea2698981939708201
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-11 14:24:03 +0100

    ghrunner_install auntomation

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -57,13 +57,13 @@ sudo chown -R $USER:$USER "$RUNNER_DIR"
 # Run the configuration script as the user (not with sudo)
 sudo -u mosesugwu bash <<EOF
 cd $RUNNER_DIR
-#./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name ghrunner-vm02 --labels self-hosted,Linux,X64,ghrunner-vm02 --runnergroup Default
-./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN <<EOL
-mosesugwu Default Runner Group
-ghrunner-vm02
-self-hosted,Linux,X64,ghrunner-vm02
-_work
-EOL
+./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name ghrunner-vm02 --labels self-hosted,Linux,X64,ghrunner-vm02 --runnergroup Default
+# ./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN <<EOL
+# mosesugwu Default Runner Group
+# ghrunner-vm02
+# self-hosted,Linux,X64,ghrunner-vm02
+# _work
+# EOL
 EOF
 
 ./run.sh &
