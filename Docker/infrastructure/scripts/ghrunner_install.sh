aa7311043145154c4a69db44d47684273b6902ba
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-17 13:04:29 +0100

    automation of ghrunner

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -55,13 +55,9 @@ echo "Running GitHub Actions runner configuration"
 sudo chown -R $USER:$USER "$RUNNER_DIR"
 
 # Run the configuration script as the user (not with sudo)
-sudo -u $USER bash -c "
-  cd $RUNNER_DIR
-  ./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name ghrunner-vm02 --labels self-hosted,Linux,X64,ghrunner-vm02 --runnergroup Default
-"
-# sudo -u mosesugwu bash <<EOF
-# cd $RUNNER_DIR
-# ./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name ghrunner-vm02 --labels self-hosted,Linux,X64,ghrunner-vm02 --runnergroup Default
+sudo -u mosesugwu bash <<EOF
+cd $RUNNER_DIR
+./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name ghrunner-vm02 --labels self-hosted,Linux,X64,ghrunner-vm02 --runnergroup Default
 # ./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN <<EOL
 # mosesugwu Default Runner Group
 # ghrunner-vm02
