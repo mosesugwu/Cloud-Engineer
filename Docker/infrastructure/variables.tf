630413026673bf92b6d218d33d1f6d25ee5330e6
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-17 15:23:51 +0100

    automation of ghrunner

--- Docker/infrastructure/variables.tf
+++ Docker/infrastructure/variables.tf
@@ -63,7 +63,7 @@ variable "custom_data_vm_2" {
 #   type        = string
 # }
 
-variable "TOKEN" {
+variable "GITHUB_PAT_TOKEN" {
   type        = string
   description = "GitHub Personal Access Token"
 }
