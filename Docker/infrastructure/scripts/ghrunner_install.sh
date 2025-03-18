bcc61d3e4cbc965f3c8387898795e5c81c00bb26
Moses Ugwu <ugwumoses1992@gmail.com>
2025-03-18 13:34:58 +0100

    automation of ghrunner

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -19,8 +19,7 @@ OWNER="mosesugwu"
 REPO="Cloud-Engineer"
 USER_HOME="/home/mosesugwu"
 USER="mosesugwu"
-RUNNER_DIR="/home/mosesugwu/actions-runner"
-#RUNNER_DIR="/actions-runner"
+RUNNER_DIR="/actions-runner"
 
 
 # Debug: Print variables
@@ -38,8 +37,7 @@ echo $PWD
 
 curl -o actions-runner-linux-x64-2.322.0.tar.gz -L "${RUNNER_URL}"
 echo "${RUNNER_SHA}  actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
-tar xzf actions-runner-linux-x64-2.322.0.tar.gz
-#tar xzf "${RUNNER_TAR}"
+tar xzf "${RUNNER_TAR}"
 
 curl -L -X POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" \
